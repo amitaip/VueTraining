74a7d6db21acf2af675630197b58e4761ed59750
Amitai Palmon <amitaip@pangea-it.com>
2020-07-06 16:33:48 +0300

    Add project files.

---
+++ VuejsApp4/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <Home msg="Hello world!" />
+        <Register/>
+    </div>
+</template>
+
+<script>
+    import Home from './components/Home.vue';
+    import Register from './components/Register.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            Home,
+            Register
+        }
+    };
+</script>
+
+<style>
+</style>
+
---
+++ VuejsApp4/src/components/Register.vue
@@ -0,0 +1,34 @@
+<template>
+    <table>
+        <tr>
+            <td><p>First Name: </p></td>
+            <td><input type="text" v-model="firstName" placeholder="type your first name"/></td>
+        </tr>
+        <tr>
+            <td><p>Last Name: </p></td>
+            <td><input type="text" v-model="lastName" placeholder="type your last name"/></td>
+        </tr>
+        <tr>
+            <td><p>Email: </p></td>
+            <td><input type="email" v-model="eMail" placeholder="type your email address"/></td>
+        </tr>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: 'Register',
+        data() {
+            return {
+                firstName: '',
+                lastName: '',
+                eMail: ''
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
+
